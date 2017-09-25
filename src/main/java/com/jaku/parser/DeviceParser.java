82318b09630d70532218bb219bf520bf4f8a1010
William Seemann <wseemann@gmail.com>
2017-09-24 23:28:58 -0700

    Add null check to prevent crash

--- src/main/java/com/jaku/parser/DeviceParser.java
+++ src/main/java/com/jaku/parser/DeviceParser.java
@@ -28,32 +28,32 @@ public class DeviceParser extends JakuParser {
             document = (Document) builder.build(new StringReader(response.getBody()));
             Element rootNode = document.getRootElement();
 
-            device.setUdn(rootNode.getChild("udn").getValue());
-            device.setSerialNumber(rootNode.getChild("serial-number").getValue());
-            device.setDeviceId(rootNode.getChild("device-id").getValue());
-            device.setVendorName(rootNode.getChild("vendor-name").getValue());
-            device.setModelNumber(rootNode.getChild("model-number").getValue());
-            device.setModelName(rootNode.getChild("model-name").getValue());
-            device.setWifiMac(rootNode.getChild("wifi-mac").getValue());
-            device.setEthernetMac(rootNode.getChild("ethernet-mac").getValue());
-            device.setNetworkType(rootNode.getChild("network-type").getValue());
-            device.setUserDeviceName(rootNode.getChild("user-device-name").getValue());
-            device.setSoftwareVersion(rootNode.getChild("software-version").getValue());
-            device.setSoftwareBuild(rootNode.getChild("software-build").getValue());
-            device.setSecureDevice(rootNode.getChild("secure-device").getValue());
-            device.setLanguage(rootNode.getChild("language").getValue());
-            device.setCountry(rootNode.getChild("country").getValue());
-            device.setLocale(rootNode.getChild("locale").getValue());
-            device.setTimeZone(rootNode.getChild("time-zone").getValue());
-            device.setTimeZoneOffset(rootNode.getChild("time-zone-offset").getValue());
-            device.setPowerMode(rootNode.getChild("power-mode").getValue());
-            device.setDeveloperEnabled(rootNode.getChild("developer-enabled").getValue());
-            device.setKeyedDeveloperId(rootNode.getChild("keyed-developer-id").getValue());
-            device.setSearchEnabled(rootNode.getChild("search-enabled").getValue());
-            device.setVoiceSearchEnabled(rootNode.getChild("voice-search-enabled").getValue());
-            device.setNotificationsEnabled(rootNode.getChild("notifications-enabled").getValue());
-            device.setNotificationsFirstUse(rootNode.getChild("notifications-first-use").getValue());
-            device.setHeadphonesConnected(rootNode.getChild("headphones-connected").getValue());
+            device.setUdn(checkValue(rootNode.getChild("udn")));
+            device.setSerialNumber(checkValue(rootNode.getChild("serial-number")));
+            device.setDeviceId(checkValue(rootNode.getChild("device-id")));
+            device.setVendorName(checkValue(rootNode.getChild("vendor-name")));
+            device.setModelNumber(checkValue(rootNode.getChild("model-number")));
+            device.setModelName(checkValue(rootNode.getChild("model-name")));
+            device.setWifiMac(checkValue(rootNode.getChild("wifi-mac")));
+            device.setEthernetMac(checkValue(rootNode.getChild("ethernet-mac")));
+            device.setNetworkType(checkValue(rootNode.getChild("network-type")));
+            device.setUserDeviceName(checkValue(rootNode.getChild("user-device-name")));
+            device.setSoftwareVersion(checkValue(rootNode.getChild("software-version")));
+            device.setSoftwareBuild(checkValue(rootNode.getChild("software-build")));
+            device.setSecureDevice(checkValue(rootNode.getChild("secure-device")));
+            device.setLanguage(checkValue(rootNode.getChild("language")));
+            device.setCountry(checkValue(rootNode.getChild("country")));
+            device.setLocale(checkValue(rootNode.getChild("locale")));
+            device.setTimeZone(checkValue(rootNode.getChild("time-zone")));
+            device.setTimeZoneOffset(checkValue(rootNode.getChild("time-zone-offset")));
+            device.setPowerMode(checkValue(rootNode.getChild("power-mode")));
+            device.setDeveloperEnabled(checkValue(rootNode.getChild("developer-enabled")));
+            device.setKeyedDeveloperId(checkValue(rootNode.getChild("keyed-developer-id")));
+            device.setSearchEnabled(checkValue(rootNode.getChild("search-enabled")));
+            device.setVoiceSearchEnabled(checkValue(rootNode.getChild("voice-search-enabled")));
+            device.setNotificationsEnabled(checkValue(rootNode.getChild("notifications-enabled")));
+            device.setNotificationsFirstUse(checkValue(rootNode.getChild("notifications-first-use")));
+            device.setHeadphonesConnected(checkValue(rootNode.getChild("headphones-connected")));
 
         } catch (JDOMException ex) {
             ex.printStackTrace();
@@ -63,4 +63,12 @@ public class DeviceParser extends JakuParser {
 
         return device;
 	}
+	
+	private String checkValue(Element element) {
+        if (element == null) {
+            return null;
+        }
+
+        return element.getValue();
+    }
 }
