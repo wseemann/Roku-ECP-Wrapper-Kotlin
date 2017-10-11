b3969bb660133f70130b33d2a105df4cd026e87d
William Seemann <wseemann@gmail.com>
2017-10-10 22:40:04 -0700

    Refactor search code, enforce type parameter

--- src/main/java/com/jaku/api/SearchRequests.java
+++ src/main/java/com/jaku/api/SearchRequests.java
@@ -3,6 +3,7 @@ package com.jaku.api;
 import java.io.IOException;
 
 import com.jaku.core.JakuRequest;
+import com.jaku.core.SearchTypeValues;
 import com.jaku.request.SearchRequest;
 
 public class SearchRequests {
@@ -11,7 +12,7 @@ public class SearchRequests {
 		
 	}
 	
-	public static final void searchRequest(String url, String keyword, String title, String type,
+	public static final void searchRequest(String url, String keyword, String title, SearchTypeValues type,
 			String tmsid, Integer season, Boolean showUnavailable, Boolean matchAny,
 			Long providerId, String provider, Boolean launch) throws IOException {
 		SearchRequest searchRequest = new SearchRequest(url, keyword, title, type,
