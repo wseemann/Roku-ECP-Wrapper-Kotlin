efffacd0007341ff2c54bb298d11bdc81488d3a8
William Seemann <wseemann@gmail.com>
2023-11-25 00:05:38 -0600

    Update example code to reflect new usage

--- examples/JakuTest.java
+++ examples/JakuTest.java
@@ -1,23 +1,21 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import javax.imageio.ImageIO;
-
-import com.jaku.api.KeyRequests;
-import com.jaku.api.LaunchRequests;
-import com.jaku.api.QueryRequests;
+import com.jaku.api.*;
 import com.jaku.core.KeypressKeyValues;
 import com.jaku.model.Channel;
 import com.jaku.model.Device;
 
 public class JakuTest {
 
-	private static final String ROKU_DEVICE_IP_ADDRESS = "<device ip address>";
-	
-	public static void main(String [] args) {		
+	private static final String ROKU_DEVICE_IP_ADDRESS = "";
+	private static RokuDevice rokuDevice;
+
+	public static void main(String [] args) {
 		try {
+			RokuDeviceFactory rokuDeviceFactory = new RokuDeviceFactoryImpl();
+			rokuDevice = rokuDeviceFactory.create(ROKU_DEVICE_IP_ADDRESS);
+
 			testKeypress();
 			testKeydown();
 			testKeyup();
@@ -30,51 +28,51 @@ public class JakuTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void testKeypress() throws IOException {
-		KeyRequests.keypressRequest(ROKU_DEVICE_IP_ADDRESS, KeypressKeyValues.INFO);
+		rokuDevice.keyPressRequest(KeypressKeyValues.INFO);
 	}
-	
+
 	private static void testKeydown() throws IOException {
-		KeyRequests.keydownRequest(ROKU_DEVICE_IP_ADDRESS, KeypressKeyValues.INFO);
+		rokuDevice.keyDownRequest(KeypressKeyValues.INFO);
 	}
-	
+
 	private static void testKeyup() throws IOException {
-		KeyRequests.keyupRequest(ROKU_DEVICE_IP_ADDRESS, KeypressKeyValues.INFO);
+		rokuDevice.keyUpRequest(KeypressKeyValues.INFO);
 	}
-	
+
 	private static void queryApps() throws IOException {
-		List<Channel> channels = QueryRequests.queryAppsRequest(ROKU_DEVICE_IP_ADDRESS);
-		
+		List<Channel> channels = rokuDevice.queryAppsRequest();
+
 		for (Channel channel: channels) {
 			System.out.println(channel.getTitle());
 		}
 	}
-	
+
 	private static void queryActiveApp() throws IOException {
-		List<Channel> channels = QueryRequests.queryActiveAppRequest(ROKU_DEVICE_IP_ADDRESS);
-		
+		List<Channel> channels = rokuDevice.queryActiveAppRequest();
+
 		for (Channel channel: channels) {
 			System.out.println(channel.getId());
 		}
 	}
-	
+
 	private static void queryDeviceInfo() throws IOException {
-		Device device = QueryRequests.queryDeviceInfo(ROKU_DEVICE_IP_ADDRESS);
-		
+		Device device = rokuDevice.queryDeviceInfo();
+
 		System.out.println(device.getCountry());
 	}
-	
+
 	private static void launchAppId() throws IOException {
-		LaunchRequests.launchAppIdRequest(ROKU_DEVICE_IP_ADDRESS, "1457");
+		rokuDevice.launchAppIdRequest("1457");
 	}
-	
+
 	private static void queryIcon() throws IOException {
-		BufferedImage bufferedImage = QueryRequests.queryIconRequest(ROKU_DEVICE_IP_ADDRESS, "1457");
-		
-		System.out.println(bufferedImage.getWidth());
-		
-		File outputfile = new File("<some path>");
-	    ImageIO.write(bufferedImage, "jpeg", outputfile);
+		byte [] data = rokuDevice.queryIconRequest("1457");
+
+		//System.out.println(bufferedImage.getWidth());
+
+		//File outputfile = new File("<some path>");
+		//ImageIO.write(bufferedImage, "jpeg", outputfile);
 	}
 }
