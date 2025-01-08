b2049e7163241e4096268b08a9f5d00d20277b5b
William Seemann <wseemann@gmail.com>
2025-01-07 23:50:43 -0600

    Fix formatting

--- src/main/java/com/wseemann/ecp/core/ECPRequest.kt
+++ src/main/java/com/wseemann/ecp/core/ECPRequest.kt
@@ -10,12 +10,14 @@ import okhttp3.Request
 import org.jdom2.JDOMException
 import java.io.IOException
 
-internal abstract class ECPRequest<T>(private val url: String,
-				      private val useRequestCache: Boolean = false) {
+internal abstract class ECPRequest<T>(
+    private val url: String,
+    private val useRequestCache: Boolean = false
+) {
 
     private var http_request: Request? = null
 
-    constructor(url: String): this(url, false)
+    constructor(url: String) : this(url, false)
 
     protected fun getUrl(): String {
         return url
@@ -36,12 +38,12 @@ internal abstract class ECPRequest<T>(private val url: String,
                 val request = DiscoveryRequest(url)
                 return ECPResponse(generateResponseData(request.send().data, getParser()))
             } else {
-		val http_manager = HttpManager.getInstance()
-		if (http_request == null)
-		    http_request = http_manager.buildRequest(url, getMethod(), useRequestCache)
-		val responseBody = http_manager.execute(http_request!!)
-		if (responseBody == null)
-		    return null
+                val httpManager = HttpManager.getInstance()
+
+                if (http_request == null) {
+                    http_request = httpManager.buildRequest(url, getMethod(), useRequestCache)
+                }
+                val responseBody = httpManager.execute(http_request!!) ?: return null
 
                 val body = responseBody.bytes()
                 debug("ECP request response: " + String(body))
--- src/main/java/com/wseemann/ecp/core/HttpManager.kt
+++ src/main/java/com/wseemann/ecp/core/HttpManager.kt
@@ -12,49 +12,49 @@ import okhttp3.RequestBody.Companion.toRequestBody
 internal class HttpManager private constructor() {
 
     private val okHttpClient = OkHttpClient.Builder()
-	.connectionPool(ConnectionPool(0, 1, TimeUnit.SECONDS))
-	.connectTimeout(2000, TimeUnit.MILLISECONDS)
-	.readTimeout(2000, TimeUnit.MILLISECONDS)
-	.build()
+        .connectionPool(ConnectionPool(0, 1, TimeUnit.SECONDS))
+        .connectTimeout(2000, TimeUnit.MILLISECONDS)
+        .readTimeout(2000, TimeUnit.MILLISECONDS)
+        .build()
 
     private val requestCache = HashMap<String, Request>()
 
-    public fun buildRequest(url: String, method: String, useRequestCache: Boolean): Request {
-	if (useRequestCache) {
-	    val request = requestCache.get(url)
-	    if (request != null)
-		return request
-	}
+    fun buildRequest(url: String, method: String, useRequestCache: Boolean): Request {
+        if (useRequestCache) {
+            val request = requestCache[url]
+            if (request != null) {
+                return request
+            }
+        }
+
         val body = if (method == "POST") "".toRequestBody() else null
-	val request = Request.Builder()
+
+        val request = Request.Builder()
             .addHeader("User-Agent", "Roku-ECP-Wrapper-Kotlin")
             .url(url.toHttpUrl())
             .method(method, body)
             .build()
-	if (useRequestCache) {
-	    requestCache.put(url, request)
-	}
-	return request
+
+        if (useRequestCache) {
+            requestCache[url] = request
+        }
+
+        return request
     }
 
-    public fun execute(request: Request): ResponseBody? {
-	val call = okHttpClient.newCall(request)
-	val response = call.execute()
-	return response.body
+    fun execute(request: Request): ResponseBody? {
+        val call = okHttpClient.newCall(request)
+        val response = call.execute()
+        return response.body
     }
 
-    public fun execute(url: String, method: String, useRequestCache: Boolean): ResponseBody? {
-	val request = buildRequest(url, method, useRequestCache)
-	return execute(request)
-    }
-
-    companion object {
-
-	// Volatile modifier is necessary to preserve execution order amoung threads
-        @Volatile private var instance: HttpManager? = null
+    internal companion object {
+        // Volatile modifier is necessary to preserve execution order among threads
+        @Volatile
+        private var instance: HttpManager? = null
 
         fun getInstance() =
-            instance ?: synchronized(this) { // synchronized to avoid concurrency problems
+            instance ?: synchronized(this) {
                 instance ?: HttpManager().also { instance = it }
             }
     }
